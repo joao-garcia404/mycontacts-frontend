66d5fce0e33e37dcab69a66356cfcb4e844fea4c
Joao Vitor Garcia <jvdasilvaeirasgarcia@gmail.com>
2023-01-21 11:34:50 -0300

    Feat: Listing contacts and creating http client

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,83 +1,117 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
+
 import { Link } from 'react-router-dom';
+
+import Loader from '../../components/Loader';
 
 import arrowIcon from '../../assets/images/icons/arrow.svg';
 import editIcon from '../../assets/images/icons/edit.svg';
 import trashIcon from '../../assets/images/icons/trash.svg';
+
+import ContactsService from '../../services/ContactsService';
 
 import {
   Container,
   InputSearchContainer,
   Card,
   Header,
-  ListContainer,
+  ListHeader,
 } from './styles';
 
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(true);
   const [contacts, setContacts] = useState([]);
+  const [orderBy, setOrderBy] = useState('asc');
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const filteredContacts = useMemo(() => contacts.filter(
+    (contact) => contact.name.toLowerCase().includes(searchTerm.toLowerCase()),
+  ), [contacts, searchTerm]);
 
   useEffect(() => {
-    fetch('http://localhost:3333/contacts')
-      .then(async (response) => {
-        const data = await response.json();
+    async function loadContacts() {
+      try {
+        setIsLoading(true);
 
-        setContacts(data);
-      });
-  }, []);
+        const contactsList = await ContactsService.listContacts(orderBy);
 
-  console.log(contacts);
+        setContacts(contactsList);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    loadContacts();
+  }, [orderBy]);
+
+  function handleChangeSearchTerm(event) {
+    setSearchTerm(event.target.value);
+  }
+
+  function handleToggleOrderBy() {
+    setOrderBy((prevState) => (prevState === 'asc' ? 'desc' : 'asc'));
+  }
 
   return (
     <Container>
+      <Loader isLoading={isLoading} />
+
       <InputSearchContainer>
-        <input type="text" placeholder="Pesquise pelo nome" />
+        <input
+          type="text"
+          placeholder="Pesquise pelo nome"
+          value={searchTerm}
+          onChange={handleChangeSearchTerm}
+        />
       </InputSearchContainer>
 
       <Header>
         <strong>
-          {contacts.length}
+          {filteredContacts.length}
           {' '}
-          {contacts.length === 1 ? 'contato' : 'contatos'}
+          {filteredContacts.length === 1 ? 'contato' : 'contatos'}
         </strong>
 
         <Link to="/new">Novo contato</Link>
       </Header>
 
-      <ListContainer>
-        <header>
-          <button type="button">
+      {filteredContacts.length > 0 && (
+        <ListHeader orderBy={orderBy}>
+          <button type="button" onClick={handleToggleOrderBy}>
             <span>Nome</span>
             <img src={arrowIcon} alt="Arrow" />
           </button>
-        </header>
+        </ListHeader>
+      )}
 
-        {contacts.map((contact) => (
-          <Card key={contact.id}>
-            <div className="info">
-              <div className="contact-name">
-                <strong>{contact.name}</strong>
+      {filteredContacts.map((contact) => (
+        <Card key={contact.id}>
+          <div className="info">
+            <div className="contact-name">
+              <strong>{contact.name}</strong>
 
-                {contact.category_name && (
-                  <small>{contact?.category_name}</small>
-                )}
-              </div>
-
-              <span>{contact.email}</span>
-              <span>{contact.phone}</span>
+              {contact.category_name && (
+                <small>{contact?.category_name}</small>
+              )}
             </div>
 
-            <div className="actions">
-              <Link to={`/edit/${contact.id}`}>
-                <img src={editIcon} alt="Edit contact" />
-              </Link>
+            <span>{contact.email}</span>
+            <span>{contact.phone}</span>
+          </div>
 
-              <button type="button">
-                <img src={trashIcon} alt="Delete contact" />
-              </button>
-            </div>
-          </Card>
-        ))}
-      </ListContainer>
+          <div className="actions">
+            <Link to={`/edit/${contact.id}`}>
+              <img src={editIcon} alt="Edit contact" />
+            </Link>
+
+            <button type="button">
+              <img src={trashIcon} alt="Delete contact" />
+            </button>
+          </div>
+        </Card>
+      ))}
     </Container>
   );
 }
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -52,24 +52,26 @@ export const Header = styled.header`
   }
 `;
 
-export const ListContainer = styled.div`
+export const ListHeader = styled.header`
   margin-top: 24px;
+  margin-bottom: 8px;
 
-  header {
-    margin-bottom: 8px;
+  button {
+    background: transparent;
+    border: none;
 
-    button {
-      background: transparent;
-      border: none;
+    display: flex;
+    align-items: center;
+    gap: 8px;
 
-      display: flex;
-      align-items: center;
-      gap: 8px;
+    span {
+      font-weight: bold;
+      color: ${({ theme }) => theme.colors.primary.main};
+    }
 
-      span {
-        font-weight: bold;
-        color: ${({ theme }) => theme.colors.primary.main};
-      }
+    img {
+      transform: ${({ orderBy }) => (orderBy === 'asc' ? 'rotate(180deg)' : 'rotate(0deg)')};
+      transition: all 0.2s ease-in;
     }
   }
 `;
