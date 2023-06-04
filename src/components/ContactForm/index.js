413b467d295490c48578b6dd8dd2673abe244030
Joao Garcia <jvdasilvaeirasgarcia@gmail.com>
2023-06-03 22:37:16 -0300

    Refactor: HTTP Client

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -16,7 +16,7 @@ import formatPhone from '../../utils/formatPhone';
 
 import { Form, ButtonContainer } from './styles';
 
-export default function ContactForm({ buttonLabel }) {
+export default function ContactForm({ buttonLabel, onSubmit }) {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [phone, setPhone] = useState('');
@@ -31,14 +31,14 @@ export default function ContactForm({ buttonLabel }) {
 
   const isFormValid = (name && errors.length === 0);
 
-  async function handleSubmit(event) {
+  function handleSubmit(event) {
     event.preventDefault();
 
-    console.log({
+    onSubmit({
       name,
       email,
       phone: phone.replace(/\D/g, ''),
-      category: categoryId,
+      categoryId,
     });
   }
 
@@ -134,4 +134,5 @@ export default function ContactForm({ buttonLabel }) {
 
 ContactForm.propTypes = {
   buttonLabel: PropTypes.string.isRequired,
+  onSubmit: PropTypes.func.isRequired,
 };
--- src/services/utils/HttpClient.js
+++ src/services/utils/HttpClient.js
@@ -1,28 +1,60 @@
 import APIError from '../../errors/ApiError';
-import delay from '../../utils/delay';
 
 class HttpClient {
   constructor(baseURL) {
     this.baseURL = baseURL;
   }
 
-  async get(path) {
-    await delay(500);
+  get(path, options) {
+    return this.makeRequest(path, {
+      method: 'GET',
+      headers: options?.headers,
+    });
+  }
 
-    const response = await fetch(`${this.baseURL}${path}`);
+  post(path, options) {
+    return this.makeRequest(
+      path,
+      {
+        method: 'POST',
+        body: options?.body,
+        headers: options?.headers,
+      },
+    );
+  }
+
+  async makeRequest(path, options) {
+    const headers = new Headers();
+
+    if (options.body) {
+      headers.append('Content-Type', 'application/json');
+    }
+
+    if (options?.headers) {
+      Object.entries(options.headers).forEach(([key, value]) => {
+        headers.append(key, value);
+      });
+    }
+
+    const response = await fetch(`${this.baseURL}${path}`, {
+      method: options.method,
+      body: JSON.stringify(options.body),
+      headers,
+    });
+
     const contentType = response.headers.get('Content-Type');
 
-    let body = null;
+    let responseBody = null;
 
     if (contentType.includes('application/json')) {
-      body = await response.json();
+      responseBody = await response.json();
     }
 
     if (response.ok) {
-      return body;
+      return responseBody;
     }
 
-    throw new APIError(response, body);
+    throw new APIError(response, responseBody);
   }
 }
 
